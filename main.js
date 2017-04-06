e512f749e054579ee092df409c0bd79fd828a6cc
Bartek <fitheak01@gmail.com>
2017-04-06 20:54:19 +0200

    Working on active section highlight

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ document.addEventListener('scroll', setNavbar);
 document.querySelectorAll('.nav-list-item a').forEach(function(item){
     item.addEventListener('click', function(e) {
         e.preventDefault();
-        scrollToElement(this);
+        scrollToElement(this.getAttribute('href'));
     })
 });
 
 document.querySelector('.btn-scroll').addEventListener('click', function(e){
     e.preventDefault();
-    scrollToElement(this);
+    scrollToElement(this.getAttribute('href'));
 });
 
 document.querySelector('.nav-content a').addEventListener('click', function(e){
     e.preventDefault();
-    scrollToElement(this);
+    scrollToElement(this.getAttribute('href'));
 });
 
 function setNavbar() {
@@ -28,13 +28,24 @@ function setNavbar() {
     }
 }
 
-var scrollToElement = function(currentElement) {
-    var target = currentElement.getAttribute('href');
+var setActiveSection = function(){
+    var linkNavList = document.querySelectorAll('.nav-list li a');
+    var length = linkNavList.length;
+    var currentSection = location.hash;
+    for(var i=0; i< length;i++){
+        if (linkNavList[i].getAttribute('href') == currentSection){
+            linkNavList[i].classList.add('active-item');
+        }
+        else linkNavList[i].classList.remove('active-item');
+    }
+}
 
+var scrollToElement = function(target) {
     document.querySelector(target).scrollIntoView({ 
         behavior: 'smooth' 
     });
-
     location.hash = target;
+    setActiveSection();    
 }
+
 

c758cce860a1d7c57cfcbdca633eeaa8ee31b69a
Bartek <fitheak01@gmail.com>
2017-04-11 21:17:48 +0200

    Alt attributes added

--- main.js
+++ main.js
@@ -17,17 +17,26 @@ document.querySelector('.nav-content a').addEventListener('click', function(e){
     scrollToElement(this.getAttribute('href'));
 });
 
+document.querySelector('.footer a').addEventListener('click', function(e){
+    e.preventDefault();
+    scrollToElement(this.getAttribute('href'));
+});
+
 function setNavbar() {
     console.log('navbar set');
     var navigation = document.querySelector('.nav');
     var scroll = document.documentElement.scrollTop || document.body.scrollTop; 
     var scrollBtn = document.querySelector('.btn-scroll');
-  
+    
+    setActiveSectionOnScroll(scroll);
     if (scroll > 100) {
        navigation.classList.add('nav-scroll');
 
-       if (scroll > document.querySelector('#head').style.height - 100) {
+       if (scroll > document.querySelector('#head').clientHeight - 200) {
            scrollBtn.classList.add('hidden');
+       }
+       else {
+            scrollBtn.classList.remove('hidden');           
        }
     }
     else {
@@ -38,7 +47,7 @@ function setNavbar() {
 
 }
 
-var setActiveSection = function(){
+var setActiveSection = function() {
     var linkNavList = document.querySelectorAll('.nav-list li a');
     var length = linkNavList.length;
     var currentSection = location.hash;
@@ -48,6 +57,17 @@ var setActiveSection = function(){
         }
         else linkNavList[i].classList.remove('active-item');
     }
+}
+
+var setActiveSectionOnScroll = function(scrollPosition) {
+    var sections = document.querySelectorAll("head,section");
+    var scroll = scrollPosition;
+    debugger;
+    sections.forEach(function(section){
+        if(scroll <= section.clientHeight){
+            document.querySelector('.nav-list li a[href="' + section.getAttribute('id') + "']");
+        }
+    });
 }
 
 var scrollToElement = function(target) {

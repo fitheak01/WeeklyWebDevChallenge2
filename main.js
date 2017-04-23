b5e8d64669713aada87e7ff9777edb4c60eeeab3
Bartek <fitheak01@gmail.com>
2017-04-23 20:13:21 +0200

    fix for active section function

--- main.js
+++ main.js
@@ -12,20 +12,15 @@ document.querySelector('.btn-scroll').addEventListener('click', function(e){
     scrollToElement(this.getAttribute('href'));
 });
 
-document.querySelector('.nav-content a').addEventListener('click', function(e){
-    e.preventDefault();
-    scrollToElement(this.getAttribute('href'));
-});
-
 document.querySelector('.footer a').addEventListener('click', function(e){
     e.preventDefault();
     scrollToElement(this.getAttribute('href'));
 });
 
+
 function setNavbar() {
-    console.log('navbar set');
     var navigation = document.querySelector('.nav');
-    var scroll = document.documentElement.scrollTop || document.body.scrollTop; 
+    var scroll = window.scrollY || document.body.scrollTop; 
     var scrollBtn = document.querySelector('.btn-scroll');
     
     setActiveSectionOnScroll(scroll);
@@ -47,23 +42,12 @@ function setNavbar() {
 
 }
 
-var setActiveSection = function() {
-    var linkNavList = document.querySelectorAll('.nav-list li a');
-    var length = linkNavList.length;
-    var currentSection = location.hash;
-    // for(var i=0; i< length;i++){
-    //     if (linkNavList[i].getAttribute('href') == currentSection){
-    //         linkNavList[i].classList.add('active-item');
-    //     }
-    //     else linkNavList[i].classList.remove('active-item');
-    // }
-}
-
-var setActiveSectionOnScroll = function(scrollPosition) {
-    var sections = document.querySelectorAll("header, section");
-    var scroll = scrollPosition;
+function setActiveSectionOnScroll(scrollPosition) {
+    var sections = document.querySelectorAll("#head, section, aside");
+    var scroll = scrollPosition + 78;
     var sectionStart = 0, sectionEnd = 0;
     var navbarItem, currentSection;
+
     for(var i=0;i<sections.length;i++){
         if (i == 0) {
             sectionStart=0;
@@ -71,12 +55,13 @@ var setActiveSectionOnScroll = function(scrollPosition) {
             sectionStart += sections[i-1].clientHeight;
         }
         sectionEnd += sections[i].clientHeight;
+        currentSection = sections[i].getAttribute('id');
+        if (sections[i].tagName == "ASIDE") { 
+            continue;          
+        }
         navbarItem = document.querySelector('.nav-list li a[href="#' + sections[i].getAttribute('id') + '"]');
         if(sectionStart <= scroll && scroll <= sectionEnd){            
             navbarItem.classList.add('active-item');
-            currentSection = navbarItem.getAttribute('id');
-            location.hash = '#' + currentSection;
-            break;
         }
         else {
             navbarItem.classList.remove('active-item');
@@ -84,12 +69,11 @@ var setActiveSectionOnScroll = function(scrollPosition) {
     }
 }
 
-var scrollToElement = function(target) {
+function scrollToElement(target) {
     document.querySelector(target).scrollIntoView({ 
         behavior: 'smooth' 
     });
-    location.hash = target;
-    setActiveSection();    
+    location.hash = target;   
 }
 
 
